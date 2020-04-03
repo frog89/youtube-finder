e3fa3e837118ad2b98498fe6705b8f2bf5cd44c0
Frank Augustin <augustin.frank@gmail.com>
2020-04-03 18:17:27 +0200

    Updates

---
+++ precache-manifest.86f89a9a40af083377c913661bc86f10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395d113ae40bf8053b3a53b151feb425",
+    "url": "/youtube-finder/index.html"
+  },
+  {
+    "revision": "a5d6ee93c9aad2b86336",
+    "url": "/youtube-finder/static/css/main.26ee5179.chunk.css"
+  },
+  {
+    "revision": "75a6297daa8b201348d8",
+    "url": "/youtube-finder/static/js/2.4f73c566.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-finder/static/js/2.4f73c566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d6ee93c9aad2b86336",
+    "url": "/youtube-finder/static/js/main.2d7a98af.chunk.js"
+  },
+  {
+    "revision": "8c7c1ba190e7e53b3933",
+    "url": "/youtube-finder/static/js/runtime-main.68dfe6d8.js"
+  }
+]);
